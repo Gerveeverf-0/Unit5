364fe395e46a1c5e6fe321d0641ad252aa0f4f5b
Gerveeverf <wolforda@roverkids.org>
2025-05-13 14:21:29 -0400

    lesson 5.2 complete (not submitted)

--- UserInterface/Unit 5/Assets/Scripts/Target.cs
+++ UserInterface/Unit 5/Assets/Scripts/Target.cs
@@ -6,10 +6,13 @@ public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
     private float minSpeed = 12;
-    private float maxSpeed = 16;
+    private float maxSpeed = 18;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    private GameManager gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +22,8 @@ public class Target : MonoBehaviour
         targetRb.AddTorque(RandomTorque(), RandomTorque(),
         RandomTorque(), ForceMode.Impulse);
         transform.position = RandomSpawnPos();
+        gameManager = GameObject.Find("GameManager")
+            .GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -29,7 +34,7 @@ public class Target : MonoBehaviour
 
     Vector3 RandomForce()
     {
-        return Vector3.up * Random.Range(minSpeed, maxTorque);
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
     }
 
     float RandomTorque()
@@ -40,5 +45,18 @@ public class Target : MonoBehaviour
     Vector3 RandomSpawnPos()
     {
         return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, 
+            explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 }
