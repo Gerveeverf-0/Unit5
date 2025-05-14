9d68c90bf3312c3a8e0c40f0440d149a832b9d1b
Gerveeverf <wolforda@roverkids.org>
2025-05-14 13:53:57 -0400

    lesson 5.3 complete

--- UserInterface/Unit 5/Assets/Scripts/Target.cs
+++ UserInterface/Unit 5/Assets/Scripts/Target.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
-    private float minSpeed = 12;
+    private float minSpeed = 13;
     private float maxSpeed = 18;
     private float maxTorque = 10;
     private float xRange = 4;
@@ -49,14 +49,19 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Destroy(gameObject);
-        Instantiate(explosionParticle, transform.position, 
-            explosionParticle.transform.rotation);
-        gameManager.UpdateScore(pointValue);
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position,
+                explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+       
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad")) { gameManager.GameOver(); }
     }
 }
